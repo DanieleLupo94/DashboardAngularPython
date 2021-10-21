0be722d478e5fbbfcdc3eae74f59e90782ba2e8f
Daniele Lupo <lupodaniele94@gmail.com>
2021-10-21 20:44:50 +0200

    Init REST server in python

---
+++ RESTserver.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify, send_file
+import datetime
+import os 
+import time
+
+app = Flask(__name__)
+
+@app.route("/turnOn")
+def turnOn():
+	os.system("python3 ./controllerTuya.py EUheu1592047867994rwfxKGqBr1eTvr9 EUheu1592047867994rwfxKHdzG8hJ3Oh 44152035483fda90cb1e on")
+	return "Luce accesa",200
+
+@app.route("/turnOff")
+def tutnOff():
+	os.system("python3 ./controllerTuya.py EUheu1592047867994rwfxKGqBr1eTvr9 EUheu1592047867994rwfxKHdzG8hJ3Oh 44152035483fda90cb1e off")
+	return "Luce spenta",200
+
+@app.route("/takePic")
+def takePic():
+	now = datetime.datetime.now()
+	filename = now.strftime("%Y%m%d%H%M%S")
+	filename = "{}.png".format(filename)
+	os.system("raspistill -w 1000 -h 1000 -t 2000 -n -dt -e png -o {}".format(filename))
+	return send_file(filename, mimetype='image/gif'), 200
+	
+@app.route("/takeVideo")
+def takeVideo():
+	now = datetime.datetime.now()
+	filename = now.strftime("%Y%m%d%H%M%S")
+	video = "{}.h264".format(filename)
+	filename = "{}.mp4".format(filename)
+	os.system("raspivid -t 5000 -n -o {}".format(video))
+	os.system("MP4Box -add {} -fps 30 {}".format(video, filename))
+	return send_file(filename, mimetype='video/mp4'), 200
+
---
+++ controllerTuya.py
@@ -0,0 +1,95 @@
+import requests
+import pprint
+import time
+import sys
+
+refresh_token = ''
+access_token = ''
+idLuce = ''
+comando = ''
+
+# Impostare userName e password
+def getAccessToken():
+	print("GET AUTH-TOKEN")
+	auth = requests.post(
+		"https://px1.tuyaus.com/homeassistant/auth.do",
+		data={
+			"userName": "",
+			"password": "",
+			"countryCode": "44",
+			"bizType": "smart_life",
+			"from": "tuya",
+		},
+	).json()
+	pprint.pprint(auth)
+	global access_token
+	global refresh_token
+	access_token = auth["access_token"]
+	refresh_token = auth["refresh_token"]
+	
+def getDevices():
+	print("GET DEVICES")
+	devices = requests.post(
+    "https://px1.tuyaus.com/homeassistant/skill",
+    json={"header": {"name": "Discovery", "namespace": "discovery", "payloadVersion": 1}, "payload": {"accessToken": access_token}}).json()
+	pprint.pprint(devices)
+
+def turnOn(deviceId):
+	print("TURNING ON")
+	turnon = requests.post(
+    "https://px1.tuyaus.com/homeassistant/skill",
+    json={"header": {"name": "turnOnOff", "namespace": "control", "payloadVersion": 1}, "payload": {"accessToken": access_token, "devId": deviceId, "value":"1"}}).json()
+	pprint.pprint(turnon)
+	
+def turnOff(deviceId):
+	print("TURNING OFF")
+	turnoff = requests.post(
+	"https://px1.tuyaus.com/homeassistant/skill",
+	json={"header": {"name": "turnOnOff", "namespace": "control", "payloadVersion": 1}, "payload": {"accessToken": access_token, "devId": deviceId, "value":"0"}}).json()
+	pprint.pprint(turnoff)
+
+def refreshToken():
+	global access_token
+	global refresh_token
+	if (access_token == '' or refresh_token == ''):
+		return getAccessToken()
+	auth = requests.post(
+		"https://px1.tuyaus.com/homeassistant/access.do",
+		data={
+			"grant_type": "refresh_token",
+			"refresh_token": refresh_token,
+			"rand": 23
+		},
+	).json()
+	pprint.pprint(auth)
+	access_token = auth["access_token"]
+	refresh_token = auth["refresh_token"]
+
+def main():
+	refreshToken()
+	
+	global idLuce
+	global comando
+	if comando == 'on':
+		turnOn(idLuce)
+	else:
+		turnOff(idLuce)
+
+if len(sys.argv) != 5:
+	print("Uso: <accessToken> <refreshToken> <id> <on/off>")
+	sys.exit(2)
+
+argv = sys.argv
+access_token
+access_token = str(argv[1])
+
+refresh_token
+refresh_token = str(argv[2])
+
+idLuce
+idLuce = str(argv[3])
+
+comando
+comando = str(argv[4])
+
+main()
