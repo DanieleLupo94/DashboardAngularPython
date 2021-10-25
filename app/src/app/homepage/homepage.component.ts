139dac285ab3052885970b0485e2b270ddb9e897
Daniele Lupo <daniele.lupo@riatlas.it>
2021-10-25 15:41:24 +0200

    Fix chiamate cross origin
    Recuperando l'ip della macchina su cui si sta hostando il progetto è possibile fare chiamate "interne" al servizio REST in modo da eludere problemi di CORS e cross origin.

--- app/src/app/homepage/homepage.component.ts
+++ app/src/app/homepage/homepage.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { FileSaverService } from 'ngx-filesaver';
 import { MessageService } from 'primeng/api';
 
@@ -10,14 +11,20 @@ import { MessageService } from 'primeng/api';
 })
 export class HomepageComponent implements OnInit {
 
-  constructor(private http: HttpClient, private messageService: MessageService, private _FileSaverService: FileSaverService) { }
+  baseUrl = window.location.origin;
+
+  constructor(private http: HttpClient, private messageService: MessageService, private _FileSaverService: FileSaverService, private router: Router) { }
 
   ngOnInit(): void {
     document.getElementsByTagName('body')[0].style.backgroundColor = "#131313";
+    if (this.baseUrl.split(":").length > 2) {
+      this.baseUrl = this.baseUrl.substring(0, this.baseUrl.lastIndexOf(":"))
+    }
+    console.log("Base ", this.baseUrl);
   }
 
   accendiLuce() {
-    this.http.get("http://localhost:5000/turnOn").toPromise().then((resp: any) => {
+    this.http.get(this.baseUrl + ":5000/turnOn").toPromise().then((resp: any) => {
       this.showMessageByResponseMessage(resp);
     }).catch(resp => {
       console.error("Errore: ", resp);
@@ -26,7 +33,7 @@ export class HomepageComponent implements OnInit {
   }
 
   spegniLuce() {
-    this.http.get("http://localhost:5000/turnOff").toPromise().then((resp: any) => {
+    this.http.get(this.baseUrl + ":5000/turnOff").toPromise().then((resp: any) => {
       this.showMessageByResponseMessage(resp);
     }).catch(resp => {
       console.error("Errore: ", resp);
@@ -35,7 +42,7 @@ export class HomepageComponent implements OnInit {
   }
 
   scattaFoto() {
-    this.http.get("http://localhost:5000/takePic", { responseType: "blob" }).toPromise().then((resp: any) => {
+    this.http.get(this.baseUrl + ":5000/takePic", { responseType: "blob" }).toPromise().then((resp: any) => {
       this._FileSaverService.save(resp, "pic.png");
     }).catch(resp => {
       console.error("Errore: ", resp);
@@ -44,7 +51,7 @@ export class HomepageComponent implements OnInit {
   }
 
   registraVideo() {
-    this.http.get("http://localhost:5000/takeVideo", { responseType: "blob" }).toPromise().then((resp: any) => {
+    this.http.get(this.baseUrl + ":5000/takeVideo", { responseType: "blob" }).toPromise().then((resp: any) => {
       this._FileSaverService.save(resp, "video.mp4");
     }).catch(resp => {
       console.error("Errore: ", resp);
